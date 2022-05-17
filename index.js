bc929c6441e7a4e071e22bed796b01f8aeaf190b
kamran <1kamranmokhtari@gmail.com>
2022-05-17 20:50:12 +0430

    first commit!

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const mtproxy = require('@mtproto-org/mtproxy');
+const http = require('http')
 
 mtproxy({
     port: 1122,
     secret: '11112222333344445555666677778888'
 })
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end("Hello, World!");
+})
+
+server.listen(3000)
